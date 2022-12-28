380a59748216a255678cdbb87321d931e01715f2
Pedro Lopes <pedro.hsl2003@gmail.com>
2022-12-27 21:18:23 -0800

    Adicionar arquivos de projeto.

---
+++ 1/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _1.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Retorno(string Login, string senha)
+        {
+            ViewBag.Login = Login;
+            ViewBag.senha = senha;
+
+            return View();
+        }
+    }
+}
